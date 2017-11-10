20516bb521b55e83f449688ff355e4e06d04374e
Aries Cs <aries0d0f@gmail.com>
2017-11-10 12:55:39 +0800

    Add RWD.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
   <div id="app">
+    <header>
+      <h1>Meow 2 human</h1>
+      <p>by <a href="https://aries0d0f.me">@aries0d0f</a></p>
+    </header>
     <router-view></router-view>
   </div>
 </template>
